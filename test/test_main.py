1c5e78c460fccfb41618bf1bd7ec0e7f327a54d4
Robbie C <robbie.coomber@gmail.com>
2015-02-24 18:20:37 +0000

    Add the remaining tests for main
    Test the case where the user is not found, an unknown http return code
    happens, and the user has no repositories.

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,9 @@
+import http.client
+
+import pytest
+import requests
+
+from git_lang_guesser import exceptions
 from git_lang_guesser import main
 from git_lang_guesser import git_requester
 
@@ -21,6 +27,17 @@ expected_count = {
 expected_favourite = "Python"
 
 
+class DummyResponse(requests.Response):
+    def __init__(self, status_code, text):
+        super(DummyResponse, self).__init__()
+        self.status_code = status_code
+        self.__text = text
+
+    @property
+    def text(self):
+        return self.__text
+
+
 class TestDoGuess(object):
 
     def test_basic(self, monkeypatch, capsys):
@@ -36,3 +53,42 @@ class TestDoGuess(object):
         out, err = capsys.readouterr()
 
         assert(out.strip() == expected_favourite)
+
+    def test_user_not_found(self, monkeypatch, capsys):
+        def mock_request(username):
+            assert(username == test_username)
+            raise exceptions.RequestFailed(DummyResponse(http.client.NOT_FOUND, ""))
+        monkeypatch.setattr(git_requester, "get_public_repos_for_user", mock_request)
+
+        with pytest.raises(SystemExit):
+            main.do_guess(username=test_username, list_all=False)
+
+        out, err = capsys.readouterr()
+
+        assert(out.strip() == "User TestUser not found")
+
+    def test_unknown_error(self, monkeypatch, capsys):
+        def mock_request(username):
+            assert(username == test_username)
+            raise exceptions.RequestFailed(DummyResponse(418, "Teapot"))
+        monkeypatch.setattr(git_requester, "get_public_repos_for_user", mock_request)
+
+        with pytest.raises(SystemExit):
+            main.do_guess(username=test_username, list_all=False)
+
+        out, err = capsys.readouterr()
+
+        assert(out.strip() == "HTTP error code 418: Teapot")
+
+    def test_no_repos(self, monkeypatch, capsys):
+        def mock_request(username):
+            assert(username == test_username)
+            return []
+        monkeypatch.setattr(git_requester, "get_public_repos_for_user", mock_request)
+
+        with pytest.raises(SystemExit):
+            main.do_guess(username=test_username, list_all=False)
+
+        out, err = capsys.readouterr()
+
+        assert(out.strip() == "User does not have any public repos")
